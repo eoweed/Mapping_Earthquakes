ade35dc0b1812fdfeea8991eedf8ebd3aeef7d0b
eoweed <90293383+eoweed@users.noreply.github.com>
2022-09-08 10:41:13 -0700

    update map for toronto neighborhoods

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,18 +1,18 @@
 //---------------------------------------------------------------
 //create tile layers
 //---------------------------------------------------------------
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 maxZoom: 18,
-id: 'mapbox/light-v10',
+id: 'mapbox/streets-v11',
 accessToken: API_KEY
 });
 
 
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 maxZoom: 18,
-id: 'mapbox/dark-v10',
+id: 'mapbox/satellite-streets-v11',
 accessToken: API_KEY
 });
 
@@ -23,8 +23,8 @@ accessToken: API_KEY
 // }
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Light: light,
-    Dark: dark
+    Streets: streets,
+    Sattelite: satelliteStreets
 };
 //---------------------------------------------------------------
 
@@ -33,9 +33,9 @@ let baseMaps = {
 // create map object with center and zoom level
 // ---------------------------------------------------------------
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
-    zoom: 2,
-    layers: [light]
+    center: [43.7, -79.3],
+    zoom: 10,
+    layers: [satelliteStreets]
 })
 // ---------------------------------------------------------------
 
@@ -50,7 +50,7 @@ L.control.layers(baseMaps).addTo(map);
 //---------------------------------------------------------------
 // access toronto airport data
 //---------------------------------------------------------------
-let torontoData = "https://raw.githubusercontent.com/eoweed/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/static/data/torontoRoutes.json"
+let torontoHoods = "https://raw.githubusercontent.com/eoweed/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/static/data/torontoNeighborhoods.json"
 //---------------------------------------------------------------
 
 
@@ -78,18 +78,19 @@ let torontoData = "https://raw.githubusercontent.com/eoweed/Mapping_Earthquakes/
 // Use variable to hold style attiributes and then grb GeoJSON data
 //---------------------------------------------------------------
 let myStyle = {
-  color: "yellow",
-  weight: 2
+  color: "blue",
+  fillColor: "yellow",
+  weight: 1
 }
 
-d3.json(torontoData, function(data) {
+d3.json(torontoHoods, function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
     style: myStyle,
     onEachFeature: function(features, layer) {
-        layer.bindPopup("<h2>Airline: "+features.properties.airline+"</h2>"+
-        "<h3>Destination: "+features.properties.dst+"</h3>")
+        layer.bindPopup("<h2>AREA_S_CD: "+features.properties.AREA_S_CD+"</h2>"+
+        "<h3>AREA_NAME: "+features.properties.AREA_NAME+"</h3>")
     }
   })
   .addTo(map);
